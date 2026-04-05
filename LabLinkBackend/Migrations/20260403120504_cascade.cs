c9e9208cf06ed014892e180544710f738ac667b6
LNU <2479519@cognizant.com>
2026-04-05 23:10:57 +0530

    commit

---
+++ LabLinkBackend/Migrations/20260403120504_cascade.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LabLinkBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class cascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_Patient",
+                table: "Appointment");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryPhysicianName",
+                table: "Patient",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_Patient",
+                table: "Appointment",
+                column: "PatientId",
+                principalTable: "Patient",
+                principalColumn: "PatientId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Appointment_Patient",
+                table: "Appointment");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PrimaryPhysicianName",
+                table: "Patient",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Appointment_Patient",
+                table: "Appointment",
+                column: "PatientId",
+                principalTable: "Patient",
+                principalColumn: "PatientId");
+        }
+    }
+}
